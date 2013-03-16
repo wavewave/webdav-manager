dcf357aa866f7faf7464908bc4ffb5bfa50939fa
Ian-Woo Kim <ianwookim@gmail.com>
2013-03-16 16:19:13 -0400

    comment clean up

--- src/HEP/Storage/WebDAV/Type.hs
+++ src/HEP/Storage/WebDAV/Type.hs
@@ -31,15 +31,10 @@ data WebDAVProgram = UseCurl (Maybe FilePath)
 -- | 
 data Credential = CredDigest String String | NoCred
 
-
-        -- webdav_program :: WebDAVProgram, 
-        -- webdav_path_wget :: String,
-        -- webdav_path_cadaver :: String,
-
+-- | 
 data WebDAVConfig = WebDAVConfig { webdav_credential :: Credential 
                                  , webdav_baseurl   :: String 
                                  } 
---                     deriving (Show, Typeable, Data)
 
 data WebDAVRemoteDir = WebDAVRemoteDir {  webdav_remotedir :: FilePath } 
                        deriving (Show, Typeable, Data)
@@ -47,15 +42,3 @@ data WebDAVRemoteDir = WebDAVRemoteDir {  webdav_remotedir :: FilePath }
 
 data WebDAVCommand = Download | Upload
   deriving (Show, Typeable, Data)
-
-{- 
-newtype WebDAVServer = WebDAVServer { 
-  webdav_server_url :: String
-} deriving (Show,Typeable,Data)
-
-data WebDAVClient = WebDAVClient { 
-  webdav_client_wget :: FilePath, 
-  webdav_client_cadaver :: FilePath
-} deriving (Show,Typeable,Data)
-
--}
